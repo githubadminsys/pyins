aeefdd866df822c1899cf4412b351e7b0f2a9509
githubadminsys <153293289+githubadminsys@users.noreply.github.com>
2024-09-25 12:44:24 +0800

    Update pyins.py
    iinput 1.0.0

--- pyins.py
+++ pyins.py
@@ -1,1 +1,14 @@
-
+def iinput(cue='',var=None,error=''):
+  _input = input(str(cue))
+  try:
+    if var != None:
+      var = int(_input)
+      return int(_input)
+    else:
+      return int(_input)
+  except TypeError:
+    if error != '':
+      return str(error)
+    else:
+      return "error"
+print(iinput(cue='Input:'))
